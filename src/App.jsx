fe5403bf5edc5ed2baeb1fe0c9b36ce1b411b8e0
Adam <Adamz1865@gmail.com>
2023-12-29 18:19:08 -0500

    Began worked on header

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+import React from "react";
 
+function Header() {
+    return (
+        <div>
+            <h1>Adam Zeineddine - Web Developer</h1>
+            <p>Software you can rely on</p>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Slides.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SlideOne() {
+    return (
+        <div>
+            <h2>Code you can rely on</h2>
+        </div>
+    )
+}
+
+export default SlideOne;
