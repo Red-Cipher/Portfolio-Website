d12924373696f976658776e483fef42b56633e7b
RedCipher <Adamz1865@gmail.com>
2024-01-04 19:21:30 -0500

    Began work on the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+const urlOne = "Placeholder string"
+const urlTwo = "Placeholder string"
+const urlThree = "Placeholder string"
+const urlFour = "Placeholder string"
+
+function NavBar(props) {
+  return (
+    <ul>
+      <li href={urlOne.props}>Home</li>
+      <li href={urlTwo.props}>Projects</li>
+      <li href={urlThree.props}>About Us</li>
+      <li href={urlFour.props}>Contact Us</li>
+    </ul>
+  );
+}
+
 function Header() {
-    return <h1>Adam's Portfolio Website</h1>
+  return (
+    <>
+      <h1>Adam Zeineddine - Software Developer</h1>
+      <NavBar />
+    </>
+  );
 }
 
 export default Header;
